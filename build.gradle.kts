5d90177abfc7ac2c554a40152abeb714d0a2986a
Fabio Santos Almeida <livre.programacao@gmail.com>
2025-08-26 08:36:58 -0300

    Configure Firebase App.
    the latest is :
    "com.google.gms:google-services:4.4.3"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+buildscript {
+    dependencies {
+        // ... other project dependencies
+        // in tutorial was "com.google.gms:google-services:4.3.10" but the latest version is: "com.google.gms:google-services:4.4.3"
+        classpath("com.google.gms:google-services:4.4.3")
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-},
-buildscript {
-    dependencies {
-        // ... other project dependencies
-        classpath("com.google.gms:google-services:4.3.10")
-    }
 }
