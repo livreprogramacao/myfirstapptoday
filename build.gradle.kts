8f19b6cd0038cf0006db78ae30e287a2197bf678
Fabio Santos Almeida <livre.programacao@gmail.com>
2025-08-26 08:07:07 -0300

    Configure Firebase App.
    Add classpath qualifier to buildscript → dependencies into your app level build.gradle file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+},
+buildscript {
+    dependencies {
+        // ... other project dependencies
+        classpath("com.google.gms:google-services:4.3.10")
+    }
 }
